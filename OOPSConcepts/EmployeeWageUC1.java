84266358408623d0ab839a5d72c358746e767366
Teju199 <tejuiyer95@gmail.com>
2022-02-14 22:23:07 +0530

    EmployeeWage UC1 added

---
+++ OOPSConcepts/EmployeeWageUC1.java
@@ -0,0 +1,25 @@
+package OOPSConcepts;
+
+import java.util.Scanner;
+
+public class EmployeeWageUC1{
+    public static final int IS_PRESENT = 1;
+    public static final int IS_ABSENT = 0;
+
+    public static int empCheck() {
+        int empCheck = (int) Math.floor(Math.random() * 10 % 2);
+
+        if (empCheck == IS_PRESENT) {
+            System.out.println("Employee is Present.");
+        }
+
+        else {
+            System.out.println("Employee is Absent.");
+        }
+        return empCheck;
+    }
+
+    public static void main(String[] args) {
+        empCheck();
+    }
+}
